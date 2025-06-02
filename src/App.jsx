4aa143e23f62b1402b4def1566c74ef60a21046b
Abduvokh1D <abduvokhideurope@gmail.com>
2025-06-02 19:43:12 +0500

    Storing state in the URL, Reading and setting a Query String , Programmatic Navigation with useNavigate, <Navigate/> ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Product from "./pages/Product";
 import Homepage from "./pages/Homepage";
@@ -9,6 +9,8 @@ import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
+import Form from "./components/Form";
 
 const BASE_URL = "http://localhost:8000";
 function App() {
@@ -40,22 +42,21 @@ function App() {
         <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate replace to="cities" />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+          <Route path="cities/:id" element={<City />} />
           <Route
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="form" element={<p>List of Form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
-        <Route path="/login" element={<Login />} />
+        <Route path="login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,27 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 function Map() {
-  return <div className={styles.mapContainer}></div>;
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat") || "unknown";
+  const lng = searchParams.get("lng") || "unknown";
+
+  return (
+    <div
+      className={styles.mapContainer}
+      onClick={() => {
+        navigate("form");
+      }}
+    >
+      <h1>Map</h1>
+      <h2>
+        Position: lat: {lat},lng: {lng}
+      </h2>
+      <button onClick={() => setSearchParams({ lat: 2003, lng: 2101 })}>
+        Change Position
+      </button>
+    </div>
+  );
 }
 
 export default Map;
